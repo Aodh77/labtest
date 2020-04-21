49f63bbdae93cb26e6d6f3408262110df9fb8225
Aodh77 <aodh77@gmail.com>
2020-04-21 17:01:40 +0100

    spawn circle and random colors

---
+++ GamesTest/Assets/Scripts/changeColor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeColor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Color[] colors = new Color[3];
+
+        colors[0] = Color.green;
+        colors[1] = Color.yellow;
+        colors[2] = Color.red;
+
+        this.GetComponent<Renderer>().material.color = colors[Random.Range(0, colors.Length)];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
